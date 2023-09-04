346f4f3bf63eeb22b075b33fe14a3d47611b1a0d
Davide Bizzi <davide.bizzi@unguess.io>
2023-09-04 16:09:55 +0200

    fix urls

--- scripts/1.20.clone-tryber.sh
+++ scripts/1.20.clone-tryber.sh
@@ -12,7 +12,7 @@ if [ -d ~/dev/tryber/tryber-api ]; then
     echo "Tryber API folder exists. Skipping..."
 else
     echo "Tryber API folder does not exist. Creating..."
-    git clone git@github.com:AppQuality/tyber-api.git ~/dev/tryber/tryber-api
+    git clone git@github.com:AppQuality/tryber-api.git ~/dev/tryber/tryber-api
 fi
 
 # check if folder ~/dev/tryber/tryber-react exists
@@ -20,7 +20,7 @@ if [ -d ~/dev/tryber/tryber-react ]; then
     echo "Tryber React folder exists. Skipping..."
 else
     echo "Tryber React folder does not exist. Creating..."
-    git clone git@github.com:AppQuality/tyber-react.git ~/dev/tryber/tryber-react
+    git clone git@github.com:AppQuality/tryber-react.git ~/dev/tryber/tryber-react
 fi
 
 # check if folder ~/dev/tryber/crowd_wp_platform exists
@@ -36,5 +36,5 @@ if [ -d ~/dev/tryber/tryber-backoffice ]; then
     echo "Tryber Backoffice folder exists. Skipping..."
 else
     echo "Tryber Backoffice folder does not exist. Creating..."
-    git clone git@github.com:AppQuality/tyber-backoffice.git ~/dev/tryber/tryber-backoffice
+    git clone git@github.com:AppQuality/tryber-backoffice.git ~/dev/tryber/tryber-backoffice
 fi
