31e01be6f556859da1846d64af1b4e57a9f0f92c
Davide Bizzi <davide.bizzi@unguess.io>
2023-09-04 15:55:09 +0200

    fix clone paths

--- scripts/1.20.clone-tryber.sh
+++ scripts/1.20.clone-tryber.sh
@@ -20,7 +20,7 @@ if [ -d ~/dev/tryber/tryber-react ]; then
     echo "Tryber React folder exists. Skipping..."
 else
     echo "Tryber React folder does not exist. Creating..."
-    git clone git@github.com:AppQuality/tyber-react.git ~/dev/tryber/tryber-api
+    git clone git@github.com:AppQuality/tyber-react.git ~/dev/tryber/tryber-react
 fi
 
 # check if folder ~/dev/tryber/crowd_wp_platform exists
@@ -36,5 +36,5 @@ if [ -d ~/dev/tryber/tryber-backoffice ]; then
     echo "Tryber Backoffice folder exists. Skipping..."
 else
     echo "Tryber Backoffice folder does not exist. Creating..."
-    git clone git@github.com:AppQuality/tyber-backoffice.git ~/dev/tryber/tryber-api
+    git clone git@github.com:AppQuality/tyber-backoffice.git ~/dev/tryber/tryber-backoffice
 fi
