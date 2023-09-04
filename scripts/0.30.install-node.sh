f542e9c2337e71bf922d9dd8a66d37e5c12220f5
d-beezee <59012086+d-beezee@users.noreply.github.com>
2023-09-04 14:57:56 +0200

    Update 0.30.install-node.sh

--- scripts/0.30.install-node.sh
+++ scripts/0.30.install-node.sh
@@ -14,6 +14,9 @@ echo "Checking for nvm..."
 if ! [ -z "$(nvm -v 2>/dev/null)" ]; then
     echo "nvm is not installed. Installing..."
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 else
     echo "nvm is installed."
 fi
