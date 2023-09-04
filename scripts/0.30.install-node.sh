7914d1c3c8083c28a8937f1e527c750a34e99753
Davide Bizzi <davide.bizzi@unguess.io>
2023-09-04 15:42:34 +0200

    better check nvm

--- scripts/0.30.install-node.sh
+++ scripts/0.30.install-node.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-
 NODE_VERSION=16
 
 echo "Checking for curl..."
@@ -14,7 +13,7 @@ echo "Checking for nvm..."
 if ! [ -z "$(command -v nvm)" ]; then
     echo "nvm is not installed. Installing..."
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-    source ~/.bashrc
+    source ~/.nvm/nvm.sh
 else
     echo "nvm is installed."
 fi
@@ -22,9 +21,19 @@ fi
 echo "Checking for node..."
 if ! [ -x "$(command -v node)" ]; then
     echo "node is not installed. Installing..."
+    source ~/.nvm/nvm.sh
     nvm install $NODE_VERSION
 else
     echo "node is installed."
+fi
+
+echo "Checking for npm..."
+if ! [ -x "$(command -v npm)" ]; then
+    echo "npm is not installed. Installing..."
+    source ~/.nvm/nvm.sh
+    nvm install $NODE_VERSION
+else
+    echo "npm is installed."
 fi
 
 echo "Checking for yarn..."
